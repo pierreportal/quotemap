f339827f457ff52ffd1bcb9ab1bb400adf895249
pierre portal <pierreportal.mac@gmail.com>
2020-11-26 14:14:48 +0100

    fix: new API feedback msg

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,34 @@ const express = require("express");
 const router = express.Router();
 const Quote = require("../models/Quote");
 
+const secretToken = process.env.SECRET;
+
 router.get("/quotes", (req, res) => {
   return Quote.find({})
     .then((data) => res.json(data))
     .catch((err) => console.log(err));
 });
 
+router.get("/random-quote", (req, res) => {
+  return Quote.find({})
+    .then((result) => res.json(result[~~(Math.random() * result.length)]))
+    .catch((err) => console.log(err));
+});
+
 router.post("/add-quote", (req, res) => {
-  const { quote } = req.body;
-  console.log("quote: ", quote);
-  Quote.create({ quote }, (err, result) => {
+  const { quote, secretId } = req.body;
+
+  if (secretId !== secretToken)
+    return res.status(500).json("You don't have the right to add quotes.");
+
+  return Quote.create({ quote }, (err, result) => {
     if (err) {
-      res.send(err);
+      res.status(500).send(err);
     } else {
       console.log(result);
-      res.send(result);
+      res.status(200).json(result);
     }
   });
-  return;
 });
 
 module.exports = router;
